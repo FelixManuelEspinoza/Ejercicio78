9b9059e04cdbd34893bab7b69ba4139b9310f457
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-10-19 18:00:42 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio78/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+class Ejercicio77
+{
+    static void Main()
+    {
+        Dictionary<string, string> diccio = new Dictionary<string, string>();
+        diccio.Add("Hello", "Hola");
+        diccio["see you later"] = "hasta luego";
+
+        Console.WriteLine(diccio["Hola"]);
+
+        if (diccio.ContainsKey("Adios"))
+            Console.WriteLine(diccio["Bye"]);
